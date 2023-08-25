f4e13aff535c243f628d1f0f96046850cc49271e
Robson Ferreira dos Santos Junior <robson.santos.rs27@gmail.com>
2023-08-25 13:11:58 -0300

    27. Authorizing resources

--- app/Http/Controllers/Api/V2/TaskController.php
+++ app/Http/Controllers/Api/V2/TaskController.php
@@ -13,6 +13,11 @@ use App\Http\Resources\TaskResource;
 
 class TaskController extends Controller
 {
+    public function __construct()
+    {
+        $this->authorizeResource(Task::class);
+    }
+
     /**
      * Display a listing of the resource.
      */
